e77d59322fb83a8c5303bf1ade3df255ddf4bc8f
ealexanderb <ealexander2705@gmail.com>
2023-09-09 01:02:56 -0500

    Primer cambio

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
-driver.get("https://ecuador.patiotuerca.com/")
-search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search-list")
-search_box.send_keys("Audi A4")
+driver.get("https://sonikaecuador.com/154-guitarras")
+search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search_widget > form > input.ui-autocomplete-input")
+search_box.send_keys("Guitarra Clasica")
 
-search_button = driver.find_element(by=By.CSS_SELECTOR, value="#openSearch > div > div > div.search-ots.false > img")
+search_button = driver.find_element(by=By.CSS_SELECTOR, value="#search_widget > form > button")
 search_button.click()
 
-vehicle_cards = driver.find_elements(By.CSS_SELECTOR, "#featuredUsed > div.xl3")
+vehicle_cards = driver.find_elements(By.CSS_SELECTOR, "#js-product-list > div.products.row > div")
 
 
 for card in vehicle_cards:
     try:
-        title = card.find_element(By.CSS_SELECTOR, "div > div > div > div.card-info.card-content > div.module.tittle").text
-        kms_y_city = card.find_element(By.CSS_SELECTOR, "div > div > div > div.card-info.card-content > div.latam-secondary-text.text-lighten-2.left.vehicle-highlight").text
-        price = card.find_element(By.CSS_SELECTOR, "div > div > div > div.card-info.card-content > strong").text
+        title = card.find_element(By.CSS_SELECTOR, "div > div.product-description > h2").text
+        kms_y_city = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span.price").text
+        price = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span:nth-child(5)").text
         print(title)
         print(kms_y_city)
         print(f"${price}")
