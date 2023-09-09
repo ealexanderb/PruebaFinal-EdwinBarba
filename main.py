3882e3387086b3cb31ce362af45e8fae328c6092
ealexanderb <ealexander2705@gmail.com>
2023-09-09 01:11:26 -0500

    Segundo cambio

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
-driver.get("https://sonikaecuador.com/154-guitarras")
+driver.get("https://sonikaecuador.com/")
 search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search_widget > form > input.ui-autocomplete-input")
 search_box.send_keys("Guitarra Clasica")
 
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#search_widget > form > button")
 search_button.click()
 
-vehicle_cards = driver.find_elements(By.CSS_SELECTOR, "#js-product-list > div.products.row > div")
+guitarras = driver.find_elements(By.CSS_SELECTOR, "#js-product-list > div.products.row > div")
 
 
-for card in vehicle_cards:
+for card in guitarras:
     try:
-        title = card.find_element(By.CSS_SELECTOR, "div > div.product-description > h2").text
-        kms_y_city = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span.price").text
-        price = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span:nth-child(5)").text
-        print(title)
-        print(kms_y_city)
-        print(f"${price}")
+        Nombre_del_producto = card.find_element(By.CSS_SELECTOR, "div > div.product-description > h2").text
+        Precio = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span.price").text
+        Estado = card.find_element(By.CSS_SELECTOR, "div > div.product-description > div > span:nth-child(5)").text
+        print(Nombre_del_producto)
+        print(Precio)
+        print(Estado)
 
         coche_actual = {
-            "title": title,
-            "kms_y_city": kms_y_city,
-            "price": price
+            "Nombre_del_producto": Nombre_del_producto,
+            "Precio": Precio,
+            "Estado": Estado
         }
 
         print("++++++++++++++++++++++++++++++++")
